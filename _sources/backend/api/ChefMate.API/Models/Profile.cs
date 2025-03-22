3c01d077cbd8eb0b203138eb8da33490dc7ca63d
brice <bricegomis@gmail.com>
2025-03-22 12:09:57 +0100

    move Models to API...

---
+++ _sources/backend/api/ChefMate.API/Models/Profile.cs
@@ -0,0 +1,10 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace ChefMate.Models;
+
+[BsonIgnoreExtraElements]
+public class Profile : ModelBase
+{
+    public required string Login { get; set; }
+    public DateTimeOffset DateLastConnection { get; set; }
+}
