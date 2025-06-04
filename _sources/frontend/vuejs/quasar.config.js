0a7ba963aee2c8ecfa3ae87dc86c885484c4a9e5
brice <bricegomis@gmail.com>
2025-06-04 08:20:26 +0200

    fix publicPath

--- _sources/frontend/vuejs/quasar.config.js
+++ _sources/frontend/vuejs/quasar.config.js
@@ -51,7 +51,7 @@ module.exports = configure(function (/* ctx */) {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      publicPath: '/chefmate/',
+      publicPath: '/ChefMate/',
       // analyze: true,
       // env: {},
       // rawDefine: {}
